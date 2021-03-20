18c3727e7ac7557878d0ce25a3ba6d7775f0492f
Manu <hbiaser132@gmail.com>
2021-03-20 17:56:58 -0500

    add credentials

---
+++ src/credentials.ts
@@ -0,0 +1,6 @@
+export const userMail = 'automail.noresponder@gmail.com';
+export const passMail = 'wfklxjncfzkfccvc';
+export const firmaJWT = 'FirmaJWTTMCUnica132*'
+
+export const adminUser = 'admin@tmc.com'
+export const adminPass = 'Admin123*'
