1a0f80cb85a9321e65077ad28e56999ca65529c2
manuel <hbiaser132@gmail.com>
2021-06-27 23:28:52 -0500

    change image whatsapp

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const app_1 = __importDefault(require("./app"));
-const db_1 = require("./db/db");
-db_1.createConnection();
+// createConnection();
 app_1.default.listen(app_1.default.get('port'), () => {
     console.log(`Server at http://localhost:${app_1.default.get('port')}`);
 });
