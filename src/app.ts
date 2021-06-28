5f27e1efeec18085ddb00b91140994561e9b5f92
manuel <hbiaser132@gmail.com>
2021-06-28 00:48:09 -0500

    change image whatsapp7

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import router from './router/router';
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 7548);
 
 // middlewares
 app.use(cors());
